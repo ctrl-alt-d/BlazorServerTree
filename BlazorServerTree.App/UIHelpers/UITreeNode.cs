f71997a3d7f27c43a2454644f897ae2c342f45a9
dani herrera <ctrl.alt.d@gmail.com>
2018-08-12 13:27:39 +0200

    actions menu

--- BlazorServerTree.App/UIHelpers/UITreeNode.cs
+++ BlazorServerTree.App/UIHelpers/UITreeNode.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace BlazorServerTree.App.UIHelpers
 {
+    public class UITreeNodeAction {
+        public bool IsEsabled { get; set; } = true;
+        public string Name { get; set; }
+        public string OpenIconic { get; set; } = "oi oi-aperture";
+    }
     public class UITreeNode
     {
         public int Id { get; set; }
@@ -13,6 +19,7 @@ namespace BlazorServerTree.App.UIHelpers
         public bool IsSelected  { get; set; } = false;
         public bool ChildrenLoaded { get; set; } = false;
         public bool IsVisible  { get; set; } = true;
+        public List<UITreeNodeAction> Actions { get; set; } = new List<UITreeNodeAction>();
         public string IsExpanded_display {
             get {
                 return (IsExpanded)?"oi oi-caret-bottom":"oi oi-caret-right";
