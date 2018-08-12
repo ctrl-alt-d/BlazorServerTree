bb58d0e4baef209c89b5409d78ac266f0f228805
dani herrera <ctrl.alt.d@gmail.com>
2018-08-12 14:54:57 +0200

    add bootstrap scripts

--- BlazorServerTree.App/UIHelpers/UITree.cs
+++ BlazorServerTree.App/UIHelpers/UITree.cs
@@ -35,7 +35,5 @@ namespace BlazorServerTree.App.UIHelpers
                 processNode( Id, orderedNodes, unorderedNodes, deep+1, n.IsVisible && n.IsExpanded );
             }
         }        
-
     }
-
 }
--- BlazorServerTree.App/UIHelpers/UITreeNode.cs
+++ BlazorServerTree.App/UIHelpers/UITreeNode.cs
@@ -18,21 +18,23 @@ namespace BlazorServerTree.App.UIHelpers
         public bool IsExpanded  { get; set; } = false;
         public bool IsSelected  { get; set; } = false;
         public bool ChildrenLoaded { get; set; } = false;
+        public bool? hasChildren { get; set; } = null;
         public bool IsVisible  { get; set; } = true;
         public List<UITreeNodeAction> Actions { get; set; } = new List<UITreeNodeAction>();
         public string IsExpanded_display {
             get {
-                return (IsExpanded)?"oi oi-caret-bottom":"oi oi-caret-right";
+                if (hasChildren.HasValue && !hasChildren.Value) {
+                    return "";
+                } else {
+                    return (IsExpanded)?"oi oi-caret-bottom":"oi oi-caret-right";
+                }
+                
             }
         }
-
         public string IsSelected_display {
             get {
                 return (IsSelected)?" active ":"";
             }
         } 
-
     }
-
-
 }
